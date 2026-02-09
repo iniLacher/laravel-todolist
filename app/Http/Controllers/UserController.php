f3b90091223f74fcb46de85d91a497bba056347e
iniLacher <adijpr25@gmail.com>
2026-02-09 08:35:52 +0700

    melakukan pengetesan di usercontrollertest

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,16 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use dump;
 use Illuminate\Http\Request;
+use App\Services\UserService;
+use Illuminate\Http\Response;
+use Illuminate\Http\RedirectResponse;
 
 class UserController extends Controller
 {
+  private UserService $userService;
+  public function __construct(UserService $userService) {
+    $this->userService = $userService; 
+  }
     public function login(): \Illuminate\Http\Response {
         return response()->view('user.login', ['title' => 'Login']);
     }
     
-    public function doLogin() {
-        return 'Hi from doLogin';
+    public function doLogin(Request $request): Response | RedirectResponse {
+        $username = $request->input('username');
+        $password= $request->input('password');
+
+        //validate input 
+        if (empty ($username) || empty ($password)) {
+            return response()->view('user.login', [
+              'title' => 'Login',
+              'error' => 'Username and password are required']);
+        }
+
+        if ($this->userService->login($username, $password)) {
+          $request->session()->put('username', $username);
+          return redirect('/');
+        }
+        
+        return response()->view('user.login', [
+          'title' => 'Login',
+          'error' => 'Invalid username or password'
+        ]);
 
     }
 
--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -2,13 +2,30 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Services\UserService;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Services\Implementation\UserServiceImpl;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserControllerTest extends TestCase
 {
     public function testLogin() {
         $this->get('/login')->assertSeeText('Login')->assertSeeText('King Abdi');
+    }
+//     protected function setUp(): void
+// {
+//     parent::setUp();
+    
+//     // Force bind UserService
+//     $this->app->singleton(UserService::class, UserServiceImpl::class);
+// }
+
+    public function testLoginSuccess() {
+      $this->post('/doLogin', [
+        'username' => 'kingAbdi',
+        'password' => 'rahasia'
+      ])->assertRedirect('/')
+        ->assertSessionHas('username', 'kingAbdi');
     }
 }
