50f3a4febd5f8e3788cb6f63630fff79de751053
iniLacher <adijpr25@gmail.com>
2026-02-09 22:03:38 +0700

    inisiasi todolistservicepprovider

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\UserServiceProvider::class,
-        App\Providers\TodolistServiceProvider::class
+        App\Providers\TodolistServiceProvider::class,
 
     ],
 
